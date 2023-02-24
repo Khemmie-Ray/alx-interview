a1a4eb1991fab221ea84b03cf1499066e44ee643
Khemmie-Ray <atokemmy@gmail.com>
2023-02-24 01:07:24 +0000

    add task file

---
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+Method that determines if a given data set represents a valid UTF-8 encoding
+"""
+
+
+def validUTF8(data):
+    """
+    Check if data is a valid UTF-8 encoding
+    Returns:
+        True if data is a valid UTF-8 encoding, False otherwise.
+    """
+    for byte in data:
+        if byte >> 7 == 0:
+            continue
+        elif byte >> 5 == 0b110:
+            continue
+        elif byte >> 4 == 0b1110:
+            continue
+        elif byte >> 3 == 0b11110:
+            continue
+        else:
+            return False
+    return True
