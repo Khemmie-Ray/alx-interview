a88fd06d9746b084e39c2a610cc178e3b45cabad
Khemmie-Ray <atokemmy@gmail.com>
2023-02-27 00:40:13 +0000

    updates to task file

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -10,15 +10,22 @@ def validUTF8(data):
     Returns:
         True if data is a valid UTF-8 encoding, False otherwise.
     """
+    remaining = 0
     for byte in data:
-        if byte >> 7 == 0:
-            continue
-        elif byte >> 5 == 0b110:
-            continue
-        elif byte >> 4 == 0b1110:
-            continue
-        elif byte >> 3 == 0b11110:
-            continue
+        if remaining == 0:
+
+            if byte >> 7 == 0b0:
+                remaining = 0
+            elif byte >> 5 == 0b110:
+                remaining = 1
+            elif byte >> 4 == 0b1110:
+                remaining = 2
+            elif byte >> 3 == 0b11110:
+                remaining = 3
+            else:
+                return False
         else:
-            return False
-    return True
+            if byte >> 6 != 0b10:
+                return False
+            remaining -= 1
+    return remaining == 0
